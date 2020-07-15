fd8f5f294476b612486a29b5f80ab1e64882e250
Felipe Duran <ryuuks1996@gmail.com>
2020-07-14 23:26:41 -0400

    Adding Image Class Attributes

--- Paint2d4/src/Recursos/Imagen.java
+++ Paint2d4/src/Recursos/Imagen.java
@@ -15,10 +15,49 @@ import javafx.scene.image.ImageView;
  */
 public class Imagen {
     
-    
+    private Image puntoControl = new Image(getClass().getResourceAsStream("/Recursos/Imagenes/puntoControl.png"));
+    private Image capsulaSubrayar = new Image(getClass().getResourceAsStream("/Recursos/Imagenes/boton-08.png"));
+    private Image capsulaSubrayarSeleccionado = new Image(getClass().getResourceAsStream("/Recursos/Imagenes/boton-09.png"));
+    private Image capsulaNegrita = new Image(getClass().getResourceAsStream("/Recursos/Imagenes/boton-11.png"));
+    private Image capsulaNegritaSeleccionado = new Image(getClass().getResourceAsStream("/Recursos/Imagenes/boton-12.png"));
+    private Image capsulaCursiva = new Image(getClass().getResourceAsStream("/Recursos/Imagenes/boton-14.png"));
+    private Image capsulaCursivaSeleccionado = new Image(getClass().getResourceAsStream("/Recursos/Imagenes/boton-15.png"));
     private Image radar = new Image(getClass().getResourceAsStream("/Recursos/Imagenes/radar.png"));
+    private Image capsulaHorizontal = new Image(getClass().getResourceAsStream("/Recursos/Imagenes/boton-18.png"));
+
+    public Image getPuntoControl() {
+        return puntoControl;
+    }
+
+    public Image getCapsulaSubrayar() {
+        return capsulaSubrayar;
+    }
+
+    public Image getCapsulaSubrayarSeleccionado() {
+        return capsulaSubrayarSeleccionado;
+    }
+
+    public Image getCapsulaNegrita() {
+        return capsulaNegrita;
+    }
+
+    public Image getCapsulaNegritaSeleccionado() {
+        return capsulaNegritaSeleccionado;
+    }
+
+    public Image getCapsulaCursiva() {
+        return capsulaCursiva;
+    }
+
+    public Image getCapsulaCursivaSeleccionado() {
+        return capsulaCursivaSeleccionado;
+    }
 
     public Image getRadar() {
         return radar;
+    }
+    
+    public Image getCapsulaHorizontal() {
+        return capsulaHorizontal;
     }
 }
